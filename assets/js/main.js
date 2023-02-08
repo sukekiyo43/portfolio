4989a8656c54b3ed4e43802bc2fd7697e8d8ad8a
sukekiyo43 <teihen13@infoseek.jp>
2023-02-09 07:38:15 +0900

    ＰＣ作業完了

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,73 +2,35 @@
 /// モーダルウィンドウ制御 ///
 
 // スクロールバーの幅の取得
-let body = document.body;
+let body0 = document.body;
 let clientWidth = body.clientWidth;
 let noScrollBarWidth = clientWidth;
 let diff = 0;
 
 // モーダルウィンドウ開
-modal_open.addEventListener('click', function () {
-    modal_open_cross.classList.toggle('open');
-    modal_store_open_cross.classList.toggle('open');
-    return_circle.classList.toggle('open');
+ninja.addEventListener('click', function () {
+    body0.classList.toggle('open');
+    // スクロールバーの長さ計算
+    clientWidth = body0.clientWidth;
+    noScrollBarWidth = body.clientWidth;
+    diff = noScrollBarWidth - clientWidth;
+    if (diff > 0) {
+        body0.style['padding-right'] = diff + 'px';
+    }
+    ninja_modal.classList.toggle('open');
+    ninja_modal.classList.add('open2');
     window.setTimeout(function () {
-        modal_store.classList.toggle('open');
-        clientWidth = body.clientWidth;
-        body.classList.toggle('open');
-        noScrollBarWidth = body.clientWidth;
-        // スクロールバーの長さ計算
-        diff = noScrollBarWidth - clientWidth;
-        if (diff > 0) {
-            body.style['padding-right'] = diff + 'px';
-        }
-    }, 1000);
+        ninja_modal.classList.remove('open2');
+    }, 300);
 });
 
 // モーダルウィンドウ閉
-modal_store_open_cross.addEventListener('click', function () {
-    modal_store_open_cross.classList.toggle('open');
-    modal_open_cross.classList.toggle('open');
+back_button.addEventListener('click', function () {
+    ninja_modal.classList.toggle('open');
+    ninja_modal.classList.add('open2');
+    body0.style['padding-right'] = '0px';
     window.setTimeout(function () {
-        return_circle.classList.toggle('open');
-        modal_store.classList.toggle('open');
-        body.classList.toggle('open');
-        body.style['padding-right'] = '0px';
-    }, 1000);
-});
-
-
-/// ドロップダウンメニュー制御 ///
-
-// 上のメニューの開閉
-accordion_trigger.addEventListener('click', function () {
-    accordion_top.classList.toggle('open');
-    accordion_front_arrow.classList.toggle('open');
-});
-
-Close_button.addEventListener('click', function () {
-    accordion_top.classList.toggle('open');
-    accordion_front_arrow.classList.toggle('open');
-});
-
-// 中のメニューの開閉
-accordion_trigger2.addEventListener('click', function () {
-    accordion_middle.classList.toggle('open');
-    accordion_front_arrow2.classList.toggle('open');
-});
-
-Close_button2.addEventListener('click', function () {
-    accordion_middle.classList.toggle('open');
-    accordion_front_arrow2.classList.toggle('open');
-});
-
-// 下のメニューの開閉
-accordion_trigger3.addEventListener('click', function () {
-    accordion_bottom.classList.toggle('open');
-    accordion_front_arrow3.classList.toggle('open');
-});
-
-Close_button3.addEventListener('click', function () {
-    accordion_bottom.classList.toggle('open');
-    accordion_front_arrow3.classList.toggle('open');
+        ninja_modal.classList.remove('open2');
+        body0.classList.toggle('open');
+    }, 300);
 });
