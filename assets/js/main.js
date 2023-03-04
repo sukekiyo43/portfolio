438b899c7775eaac5b52b0069adb3d66ed8aee05
sukekiyo43 <teihen13@infoseek.jp>
2023-03-05 05:30:55 +0900

    二つ目の成果物埋め込み完了

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,30 @@
 
 /// モーダルウィンドウ制御 ///
 
-// スクロールバーの幅の取得
-let body0 = document.body;
-let clientWidth = body.clientWidth;
-let noScrollBarWidth = clientWidth;
-let diff = 0;
-
+// ギャラリーサイトモーダルウィンドウ
 // モーダルウィンドウ開
 ninja.addEventListener('click', function () {
-    // スクロールバーの長さ計算
-    clientWidth = body0.clientWidth;
-    noScrollBarWidth = body.clientWidth;
-    diff = noScrollBarWidth - clientWidth;
-    if (diff > 0) {
-        body0.style['padding-right'] = diff + 'px';
-    }
     ninja_modal.classList.toggle('open');
-    ninja_modal.classList.add('open2');
-    window.setTimeout(function () {
-        ninja_modal.classList.remove('open2');
-    }, 300);
     body0.classList.toggle('open');
-
 });
 
 // モーダルウィンドウ閉
 back_button.addEventListener('click', function () {
     ninja_modal.classList.toggle('open');
-    ninja_modal.classList.add('open2');
-    body0.style['padding-right'] = '0px';
-    window.setTimeout(function () {
-        ninja_modal.classList.remove('open2');
-        body0.classList.toggle('open');
-    }, 300);
+    body0.classList.toggle('open');
 });
+
+
+// 世界七不思議モーダルウィンドウ
+// モーダルウィンドウ開
+Seven_Wonders.addEventListener('click', function () {
+    Seven_Wonders_modal.classList.toggle('open');
+    body0.classList.toggle('open');
+});
+
+// モーダルウィンドウ閉
+back_button2.addEventListener('click', function () {
+    Seven_Wonders_modal.classList.toggle('open');
+    body0.classList.toggle('open');
+});
+
