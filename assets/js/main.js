f5f214bc4d0684ad7431b1e5178a80dafbf1dc66
sukekiyo43 <teihen13@infoseek.jp>
2023-03-08 06:28:04 +0900

    大和製作所アップ完了

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,3 +28,16 @@ back_button2.addEventListener('click', function () {
     body0.classList.toggle('open');
 });
 
+// 大和製作所モーダルウィンドウ
+// モーダルウィンドウ開
+yamato.addEventListener('click', function () {
+    yamato_modal.classList.toggle('open');
+    body0.classList.toggle('open');
+});
+
+// モーダルウィンドウ閉
+back_button3.addEventListener('click', function () {
+    yamato_modal.classList.toggle('open');
+    body0.classList.toggle('open');
+});
+
